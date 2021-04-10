24a9468261dd30b2ee82c3e314899d2457441c9b
Bryan P. Reyes <bryanpr17@gmail.com>
2021-04-11 00:11:50 +0800

    Update: Add semaphore compatibility

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const axios = require('axios').default;
+require('dotenv').config();
+
 
 const app = express();
-dotenv.config();
 
-const accountSid = process.env.ACCOUNT_SID; 
-const authToken = process.env.AUTH_TOKEN; 
-const client = require('twilio')(accountSid, authToken); 
- 
-
-app.get('/', async (req, res) => {
-    const result = await client.messages 
-    .create({ 
-       body: 'Test message. Hello World',  
-       messagingServiceSid: 'MGab190ee1f752c173cfb82b2bb726b832',      
-       to: '+639451264251' 
-     }) 
-    .then(message => message.sid)
-    .done();
+app.get('/v1/:to/:message', async (req, res) => {
+    const {to, message} = req.params;
+    let data;
+    try {
+        data = await axios.post(process.env.SEMAPHORE,{
+            apikey: process.env.API_KEY,
+            number: to,
+            message,
+        })
+        .then(resp => resp).catch(err => err);
+        console.log(data.data);
+    } catch (error) {
+        console.log(error)
+    }
 
     res.send({
-        status: 'Success',
-        message: `Message successfully sent to! ${result}`
+        status: 'success',
+        message: `Message successfully sent!`
     })
 })
+
 
 app.listen(process.env.PORT, () => console.log('Alright we start'));
